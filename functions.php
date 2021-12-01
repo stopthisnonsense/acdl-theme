f3fdea2acc7c50f5ed0fcd2cfc91d3a888370c2b
StopNonsense <anthonyjmarvin@gmail.com>
2021-12-01 15:12:39 -0500

    Set up new resource category and resources.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
- 
+
 function divichild_enqueue_scripts() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/scripts.js', array( 'jquery' ));
@@ -12,6 +12,47 @@ add_action( 'wp_enqueue_scripts', 'divichild_enqueue_scripts' );
 
 
 
+add_shortcode( 'resource_categories', 'ds_resource_categories' );
+
+function ds_resource_categories() {
+    $params = [
+        'limit' => -1,
+    ];
+
+    $resource_categories = pods( 'resource_category', $params );
+
+    if( $resource_categories->fetch() ) {
 
 
 
+        $content = '<div class="grid grid--resources">';
+        while( $resource_categories->fetch() ) {
+            $term_id = $resource_categories->display( 'term_id' );
+            $term_name = $resource_categories->display( 'name' );
+            $term_slug = $resource_categories->display( 'slug' );
+            $term_image = get_stylesheet_directory_uri() . '/images/plus.png';
+            if( !empty($resource_categories->display( 'featured_image' )) ) {
+                $term_image = $resource_categories->display( 'featured_image' );
+            }
+            $featured_image = "<img src='{$term_image}' class='grid-item__image grid-item__image--resources grid-item__image--{$term_id}'>";
+
+            // var_dump($resource_categories->fetch());
+            $content .= '<a class="grid-item grid-item--resources grid-item--' . $term_id . '" href="' . get_the_permalink( 239717 ) . '?_sft_resource_category=' . $term_slug . '">';
+
+                $content .= '<div class="grid-item__content grid-item__content--resources grid-item__content--' . $term_id . '">';
+                    if( $featured_image ) {
+                        $content .= $featured_image;
+                    }
+                    $content .= '<h2 class="grid-item__header grid-item__header--resources grid-item__header--' . $term_id . '">' . $term_name . '</h2>';
+
+                $content .= '</div>';
+
+            $content .= '</a>';
+
+            unset($featured_image);
+
+        }
+        $content .= '</div>';
+        return $content;
+    }
+}
---
+++ search-filter/results.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Search & Filter Pro
+ *
+ * Sample Results Template
+ *
+ * @package   Search_Filter
+ * @author    Ross Morsali
+ * @link      https://searchandfilter.com
+ * @copyright 2018 Search & Filter
+ *
+ * Note: these templates are not full page templates, rather
+ * just an encaspulation of the your results loop which should
+ * be inserted in to other pages by using a shortcode - think
+ * of it as a template part
+ *
+ * This template is an absolute base example showing you what
+ * you can do, for more customisation see the WordPress docs
+ * and using template tags -
+ *
+ * http://codex.wordpress.org/Template_Tags
+ *
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( $query->have_posts() )
+{
+	?>
+
+	Found <?php echo $query->found_posts; ?> Results<br />
+	Page <?php echo $query->query['paged']; ?> of <?php echo $query->max_num_pages; ?><br />
+	<?php
+	while ($query->have_posts())
+	{
+		$query->the_post();
+		$resources = pods( 'resource', get_the_ID() );
+		$resource_link = get_the_permalink();
+		if( $resources->display( 'file' ) && $resources->display('download_only') == true) {
+			$resource_link = $resources->display( 'file' );
+		}
+
+		?>
+		<div <?php post_class( 'search-filter-item' ); ?>>
+			<h2><a href="<?= $resource_link; ?>"><?php the_title(); ?></a></h2>
+			<p class="search-filter-item__date"><small><?php the_date(); ?></small></p>
+
+			<p><br /><?php the_excerpt(); ?></p>
+			<?php
+				if ( has_post_thumbnail() ) {
+					echo '<p>';
+					the_post_thumbnail("small");
+					echo '</p>';
+				}
+			?>
+			<p><?php the_category(); ?></p>
+			<p><?php the_tags(); ?></p>
+
+		</div>
+
+		<hr />
+		<?php
+	}
+	?>
+	<div class="pagination pagination--search-filter">
+		<?php
+			/* example code for using the wp_pagenavi plugin */
+			if (function_exists('wp_pagenavi'))
+			{
+				echo "<br />";
+				wp_pagenavi( array( 'query' => $query ) );
+			}
+		?>
+	</div>
+	<?php
+}
+else
+{
+	echo "No Results Found";
+}
+?>
